5bafd9c40e018136128098179113722cb65acf2e
Handipo <handipo@gmail.com>
2026-08-03 17:27:40 +0700

    update

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,7 +4,7 @@ import { defineConfig } from 'astro/config';
 // https://astro.build/config
 export default defineConfig({
     // Your exact live URL (replace with your GitHub username)
-    site: 'https://handipo2022.github.io/astro-tutorial/',
+    // site: 'https://handipo2022.github.io/astro-tutorial/',
     // Your exact repository name, prefixed with a slash
-    base: '/github.com/handipo2022/astro-tutorial',
+    // base: '/github.com/handipo2022/astro-tutorial',
 });
