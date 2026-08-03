96c45292634d89ed60b5ae54674144bcf8b5b071
Handipo <handipo@gmail.com>
2026-08-03 16:52:21 +0700

    create nojekyll file

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,5 +1,10 @@
 // @ts-check
 import { defineConfig } from 'astro/config';
-
+// https://github.com/handipo2022/astro-tutorial
 // https://astro.build/config
-export default defineConfig({});
+export default defineConfig({
+  // Your exact live URL (replace with your GitHub username)
+  site: 'https://handipo2022.github.io', 
+  // Your exact repository name, prefixed with a slash
+  base: '/github.com/handipo2022/astro-tutorial', 
+});
